6e2eae2d4a9118ce96bb1ccc9fc8aa2b6d8140a1
EnkhtuyaM <enkhtuyam1001@gmail.com>
2024-01-09 23:53:00 +0800

    react project from home

--- src/App.js
+++ src/App.js
@@ -1,3 +1,17 @@
+import Button from "./components/Button";
+import "./styles/global.css"
+
+// export default function App (){
+//   return (
+//     <div>
+//       <Button/>
+//       {/* <Button value="-" />
+//       <Button value="1" /> */}
+      
+//     </div>
+//   )
+// }
+
 function App() {
   return (
     <div>
@@ -5,3 +19,45 @@ function App() {
     </div>
   );
 }
+
+// import MyName, { DummyNews, DummyProducts } from "./utils/dummyData";
+// import Card from "./components/Card";
+// import { useState } from "react";
+
+// function useState(initialValue) {
+//   let variable = initialValue;
+
+//   function changeVariable(newVar) {
+//     variable = newVar;
+//   }
+//   // reRender
+//   return [variable, changeVariable];
+// }
+
+// export default function App() {
+//   const [count, setCount] = useState(-1);
+
+  // function incrementHandler() {
+  //   setCount(count + 1);
+  // }
+  // function decrementHandler() {
+  //   setCount(count - 1);
+  // }
+  // console.log(count);
+//   let buttonValues = ["-", "+", "1"];
+//   return (
+//     <div>
+      // <Button value="+" />
+      // <Button value="-" />
+      // <Button value="1" />
+//     </div>
+//   );
+// }
+
+// let nums = [12, 20, 30];
+// const [b, a] = nums;
+// // const a = nums[1];
+// // const b = nums[0];
+// console.log(a);
+// console.log(b);
+// console.log(nums);
---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "./Button";
+import "../styles/button.css"
+
+
+export default function Button(props){
+    const value=props.value
+
+    return (
+        <div className={props.value}>
+
+
+        </div>
+    );
+}
